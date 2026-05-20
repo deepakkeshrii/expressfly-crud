c623f3dec1099f6de22b4172f4b4e5edb3a260d3
deepak_keshri <deepakkeshri263@gmail.com>
2026-05-20 21:09:14 +0530

    -working on navbar and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
